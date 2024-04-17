a87df8b40165f5633b784c777c5ea6a5e06ca857
Pascal Perrenoud <pascal@pband.ch>
2024-04-17 12:30:33 +0200

    astar : Rewrite as function

--- astar/astar.py
+++ astar/astar.py
@@ -1,65 +1,74 @@
 from heapq import heappush, heappop
 
-class AStarGraph:
-    def __init__(self, graph):
-        self.graph = graph
 
-    def heuristic(self, node, goal):
-        # Ici, nous utilisons une simple heuristique de distance de Manhattan
-        (x1, y1) = node
-        (x2, y2) = goal
-        return abs(x1 - x2) + abs(y1 - y2)
+def search(config, weights, start, stop):
+    def inside(node):
+        return 0 <= node[0] < config["width"] and 0 <= node[1] < config["height"]
 
-    def neighbors(self, node):
+    if not inside(start) or not inside(stop):
+        print("start or stop is outside of grid :(")
+        return None
+
+    if stop in weights and weights[stop] == 'w':
+        print("Stop is a wall...")
+        return None
+
+    frontier = []
+    heappush(frontier, (0, start))
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = 0
+    cost_so_far[start] = 0
+
+    def cost(node):
+        return weights[node] if node in weights else config["default_weight"]
+
+    def neighbors(node):
         x, y = node
-        # Les voisins d'un nœud sont les nœuds adjacents dans le graphe
-        return [(nx, ny) for nx, ny in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)] if 0 <= nx < len(self.graph) and 0 <= ny < len(self.graph[0])]
+        return [(nx, ny) for nx, ny in
+                [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1),
+                 (x - 1, y - 1), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1)]
+                if inside((nx, ny))]
 
-    def cost(self, from_node, to_node):
-        return self.graph[to_node[0]][to_node[1]]
-
-    def a_star_search(self, start, goal):
-        frontier = []
-        heappush(frontier, (0, start))
-        came_from = {}
-        cost_so_far = {}
-        came_from[start] = None
-        cost_so_far[start] = 0
-
-        while frontier:
+    while True:
+        try:
             current_cost, current_node = heappop(frontier)
+            # print(f"Evaluate {current_node}")
+        except:
+            print("AStar : out of node in heap")
+            return None
 
-            if current_node == goal:
-                break
+        if current_node == stop:
+            # print("Reached end")
+            break
 
-            for next_node in self.neighbors(current_node):
-                new_cost = cost_so_far[current_node] + self.cost(current_node, next_node)
-                if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
-                    cost_so_far[next_node] = new_cost
-                    priority = new_cost + self.heuristic(next_node, goal)
-                    heappush(frontier, (priority, next_node))
-                    came_from[next_node] = current_node
+        # print(f"Neighbors: {neighbors(current_node)}")
+        for next_node in neighbors(current_node):
+            if cost(next_node) == 'w':
+                continue
+            new_cost = current_cost + cost(next_node)
+            if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
+                cost_so_far[next_node] = new_cost
+                heappush(frontier, (new_cost, next_node))
+                came_from[next_node] = current_node
 
-        path = []
-        current_node = goal
-        while current_node != start:
-            path.append(current_node)
-            current_node = came_from[current_node]
-        path.append(start)
-        path.reverse()
-        return path
+    path = []
+    current_node = stop
+
+    while current_node != start:
+        path.append(current_node)
+        current_node = came_from[current_node]
+
+    path.append(start)
+    path.reverse()
+
+    return path
 
 
-if __name__ == "__main__":
-    # Obtenez les arguments de la ligne de commande
-    graph = eval(sys.argv[1])
-    start = eval(sys.argv[2])
-    goal = eval(sys.argv[3])
+if __name__ == '__main__':
+    config = {"width": 10, "height": 8, "default_weight": 1}
+    weigths = {}
+    start = (0, 0)
+    stop = (9, 7)
 
-    # Création d'une instance de la classe AStarGraph
-    astar = AStarGraph(graph)
-
-    # Trouver le chemin le plus court
-    shortest_path = astar.a_star_search(start, goal)
-    # Pour l'instant vieux print dégeux à voir plus tard 
-    print(shortest_path)
+    print(search(config, weigths, start, stop))
