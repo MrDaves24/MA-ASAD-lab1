5641260aa2c6097676f6aed2b4180d6a3e123dc3
Pascal Perrenoud <pascal@pband.ch>
2024-04-17 12:30:10 +0200

    backend : Call astar API with requests

--- backend/server.py
+++ backend/server.py
@@ -1,29 +1,20 @@
 from flask import Flask, request, jsonify
-from subprocess import Popen, PIPE
+from requests import post
 
 app = Flask(__name__)
+astar = "http://astar:8080/"
 
-# On peut faire pour plusieurs algo si on veut la découpe ce fait ici dans le server 
-# Pour l'instant que A* et je sais pas si c'est super bien codé 
 
-@app.route('/shortest_path', methods=['POST'])
-def shortest_path():
+@app.route('/a_star', methods=['POST'])
+def a_star():
     data = request.get_json()
-    graph = data.get('graph')
-    start = data.get('start')
-    goal = data.get('goal')
 
     try:
-        container = client.containers.run('astar_algorithm', command=f'python astar.py "{graph}" "{start}" "{goal}"', detach=True)
-        container.wait()
-
-        # Récupérer vieux print
-        stdout = container.logs().decode().strip()
-
-        return jsonify({'shortest_path': stdout}), 200
-
-    except docker.errors.APIError as e:
+        result = post(astar, data)
+        return jsonify(result), 200
+    except:
         return jsonify({'error': 'An error occurred while running the algorithm.'}), 500
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3939)
+    app.run(host='0.0.0.0', port=8080)
