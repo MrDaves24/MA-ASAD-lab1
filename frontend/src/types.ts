be1adb52e4c3eb235dbc4d391a736de158210947
Pascal Perrenoud <pascal@pband.ch>
2024-04-23 21:11:51 +0200

    frontend : Add type Position to manage serialisation issues :(

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,6 @@
 export type Config = {
-    start: [number, number],
-    stop: [number, number],
+    start: Position,
+    stop: Position,
     height: number,
     width: number,
     default_weight: number
@@ -10,4 +10,34 @@ export enum Action {
     Wall,
     Weight,
     Clear,
+}
+
+export type Weight = number | 'w'
+
+export class Position {
+    constructor(private readonly _x: number, private readonly _y: number) {}
+
+    public equals(other: Position) : boolean {
+        return this._x === other._x && this._y === other._y
+    }
+    public inside(config: Config) : boolean {
+        return this._x >= 0 && this._x < config.width && this._y >= 0 && this._y < config.height
+    }
+    toString() : string {
+        return `${this._x}-${this._y}`
+    }
+    public static fromString(str: string) : Position {
+        const [x, y] = str.split('-').map(Number)
+        return new Position(x, y)
+    }
+    public get x() : number {
+        return this._x
+    }
+    public get y() : number {
+        return this._y
+    }
+
+    public toJSON() : string {
+        return this.toString()
+    }
 }
