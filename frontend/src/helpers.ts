d718b7c45e25784bd947cbb8aa8eb8761470cd1a
Pascal Perrenoud <pascal@pband.ch>
2024-04-23 21:23:38 +0200

    frontend : Implement call to API

--- frontend/src/helpers.ts
+++ frontend/src/helpers.ts
@@ -1,8 +1,24 @@
-import {type Config} from './types'
+import {API} from './const'
+import {type Config, Position, Weight} from './types'
 
-export async function callapi(weights: Map<string, number | 'w'>, config: Config) : Promise<Map<string, string>> {
-    // ←↑↓→↖↗↘↙
-    throw "todo"
+import {fetcher as fett} from 'itty-fetcher'
+
+const fetcher = fett({base: API})
+
+export async function callapi(weights: Map<string, Weight>, config: Config) : Promise<Map<string, "←"|"↑"|"↓"|"→"|"↖"|"↗"|"↘"|"↙"> | undefined> {
+    const w: (string[] | {error: string}) = await fetcher.post('/a_star', {config, weights: [...weights.entries()]})
+    if (w.error !== undefined) {
+        console.log(w.error)
+        return undefined
+    }
+    const path: Position[] = w.map(Position.fromString)
+
+    const res = new Map()
+    for (let i = 1; i < path.length - 1; i++) {
+        res.set(path[i].toString(), path[i].directionTo(path[i + 1]))
+    }
+
+    return res
 }
 
 export function range(from: number, to?: number) : Array<number> {
